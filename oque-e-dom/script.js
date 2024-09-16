d7fef0445c931c0f4066e31d86e9d7524780f25e
Pedro Henrique <164585741+PEDR0011@users.noreply.github.com>
2024-09-16 07:25:18 -0300

    oque é dom

--- oque-e-dom/script.js
+++ oque-e-dom/script.js
@@ -10,6 +10,18 @@ if(href === "http://127.0.0.1:5500/oque-e-dom/index.html") {
 // Usando o objeto doctument e o método querySelector.
 var h1 = document.querySelector("h1");
 
+function clickou() {
+  console.log("CLICK no titulo")
+}
+h1.addEventListener("click", clickou)
+
 /*
 window / location / document : Os três objetos mais usado no dom. O window é um objeto global pode chamar as propriedade e métodos sem chama o objeto window antes. Os objetos pode ter outros obejtos na sua propriedade.
 */
+
+
+// EXERCICIO
+
+console.log(window.location.href);
+
+let h2Ativo = document.querySelector(".ativo");
